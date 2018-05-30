9a67ba647de5f16ce0beb37d5afd4b62602e0a82
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2018-05-30 10:30:49 +0530

    add check for user changing their own permissions

--- functions/services/manage/permissions.js
+++ functions/services/manage/permissions.js
@@ -66,6 +66,15 @@ const validateRequestBody = (conn) => {
     return;
   }
 
+  if (conn.requester.phoneNumber === conn.req.body.phoneNumber) {
+    sendResponse(
+      conn,
+      code.forbidden,
+      'You cannot change your own permissions.'
+    );
+    return;
+  }
+
   if (!conn.req.body.permissions) {
     sendResponse(
       conn,
@@ -75,12 +84,15 @@ const validateRequestBody = (conn) => {
     return;
   }
 
+  /** The only object we need here is {...}. Anything else like an `Array`
+   * is should is not allowed.
+   */
   if (Object.prototype.toString
     .call(conn.req.body.permissions) !== '[object Object]') {
     sendResponse(
       conn,
       code.badRequest,
-      'The permissions object in request body is invalid.'
+      'The "permissions" object in request body is invalid.'
     );
     return;
   }
@@ -97,7 +109,11 @@ const app = (conn) => {
     }
 
 
-    if (!userRecord.customClaims.superUser) {
+    /** The superUser field can be `undefined', true` or `false`. So, we are
+     * explictly checking its value against a boolean. Only a `truthy`/`falsey`
+     * check will fail here.
+    */
+    if (userRecord.customClaims.superUser !== true) {
       sendResponse(conn, code.forbidden, 'You cannot perform this operation');
       return;
     }
