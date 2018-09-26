2900331e3884a76b73be28e21050073200023333
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2018-09-26 17:42:03 +0530

    Refactor

--- functions/firestore/addendum/on-create.js
+++ functions/firestore/addendum/on-create.js
@@ -24,31 +24,36 @@ const haversineDistance = (coords1, coords2) => {
   const lat1 = coords1[1];
   const lon2 = coords2[0];
   const lat2 = coords2[1];
+  /** In KM */
   const RADIUS_OF_EARTH = 6371;
 
   const x1 = lat2 - lat1;
 
   const dLat = toRad(x1);
   const x2 = lon2 - lon1;
-  const dLon = toRad(x2)
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const dLon = toRad(x2);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2)
+    + Math.cos(toRad(lat1)) * Math.cos(toRad(lat2))
+    * Math.sin(dLon / 2) * Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const dist = RADIUS_OF_EARTH * c;
 
   return dist;
 };
 
-module.exports = (addendumDoc) => {
+module.exports = (addendumDoc, context) => {
   const {
-    geopoint,
+    location,
     activityData,
   } = addendumDoc.data();
 
-  const locals = {
-    'latlng': getLatLngString(geopoint),
-  };
+  // const locals = {
+  //   'latlng': getLatLngString(location),
+  // };
+
+  console.log('context:', context);
+  console.log('getLocationUrl:', getLocationUrl(location));
 
   return Promise.resolve();
 };
