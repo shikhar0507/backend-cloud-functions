e3cfd7fe4bc0c35b0a1dec77ebd3dd14770f7722
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2018-10-26 18:04:38 +0530

    refactor

--- functions/admin/utils.js
+++ functions/admin/utils.js
@@ -61,6 +61,13 @@ const sendJSON = (conn, json, statusCode = code.ok) => {
 const sendResponse = (conn, statusCode, message = '') => {
   conn.res.writeHead(statusCode, conn.headers);
 
+  console.log({
+    message,
+    requester: conn.requester,
+    reqBody: conn.req.body,
+    url: conn.req.url,
+  });
+
   conn.res.end(JSON.stringify({
     message,
     /** 2xx codes denote success. */
--- functions/server/server.js
+++ functions/server/server.js
@@ -142,6 +142,12 @@ const handleActivitiesUrl = (conn, urlParts) => {
 const handleServicesUrl = (conn, urlParts) => {
   const resource = urlParts[2];
 
+  if (resource === 'users') {
+    require('../services/users/on-read')(conn);
+
+    return;
+  }
+
   if (resource === 'permissions') {
     require('../services/on-permissions')(conn);
 
