3f1596d1d5e97200240aa68c13bc00abaf5dac89
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2019-05-16 18:21:46 +0530

    Update /auth page with logo. Remove default color from css for body. Fix invalid function isValidEmail

--- website/js/main.js
+++ website/js/main.js
@@ -91,7 +91,7 @@ function getMobileOperatingSystem() {
 };
 
 function isValidEmail(emailString) {
-  return reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+  return /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
     .test(emailString);
 }
 
