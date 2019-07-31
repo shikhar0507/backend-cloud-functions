acb7ce1f74f780f9df3185d274da893ea5ed5217
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2019-07-31 18:31:20 +0530

    Storing array of objects instead of object of objects in profile for read response in MonthlyOnWrite

--- functions/firestore/monthly/index.js
+++ functions/firestore/monthly/index.js
@@ -21,13 +21,44 @@ module.exports = async (change, context) => {
       return Promise.resolve();
     }
 
+    const { path } = change.after.ref;
+    const parts = path.split('/');
+    const monthYearString = parts[3];
+    const [month, year] = monthYearString.split(' ');
+
+    const allMonths = {
+      'January': 0,
+      'February': 1,
+      'March': 2,
+      'April': 3,
+      'May': 4,
+      'June': 5,
+      'July': 6,
+      'August': 7,
+      'September': 8,
+      'October': 9,
+      'November': 10,
+      'December': 11,
+    };
+
+    const result = [];
+
+    Object.keys(statusObject).forEach(date => {
+      const o = Object.assign({}, {
+        office,
+        date: Number(date),
+        year: Number(year),
+        month: allMonths[month],
+      }, statusObject[date]);
+
+      result.push(o);
+    });
+
     return rootCollections
       .profiles
       .doc(phoneNumber)
       .set({
-        statusObject: {
-          [office]: statusObject,
-        }
+        statusObject: result,
       }, {
           merge: true,
         });
