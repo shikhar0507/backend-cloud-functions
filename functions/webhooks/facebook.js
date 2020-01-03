cd45c6f567761ab511ad5b9db6b0e38b1bcb4c60
Utkarsh Bhatt <utkarsh.bhatt12@gmail.com>
2020-01-03 18:18:32 +0530

    Implement facebook webhook events collection

--- functions/webhooks/facebook.js
+++ functions/webhooks/facebook.js
@@ -1,14 +1,43 @@
 'use strict';
 
-// const { rootCollections } = require('../admin/admin');
-// const { sendJSON } = require('../admin/utils');
-// const { code } = require('../admin/responses');
-// const env = require('../admin/env');
+const {rootCollections} = require('../admin/admin');
+const env = require('../admin/env');
+const momentTz = require('moment-timezone');
 
-module.exports = async conn => {
-  if (conn.req.method === 'GET') {
-    return conn.req.query['hub.challenge'];
-  }
+const storeEvents = async conn => {
+  const {date, months: month, years: year} = momentTz().toObject();
+  const ref = rootCollections.facebookEvents.doc();
+
+  console.log('ref', ref.path);
+
+  await ref.set({
+    date,
+    month,
+    year,
+    type: conn.req.body.object || null,
+    body: conn.req.body,
+    query: conn.req.query,
+    receivedAt: Date.now(),
+  });
 
   return '';
+};
+
+module.exports = async conn => {
+  // This way of verifying is perhaps not correct.
+  // We should store this event details somewhere and then
+  // verify them in the subsequent POST request.
+  // Not sure, though.
+  if (
+    conn.req.method === 'GET' &&
+    conn.req.query['hub.verify_token'] === env.fbVerificationToken
+  ) {
+    return parseInt(conn.req.query['hub.challenge']);
+  }
+
+  if (conn.req.method === 'POST') {
+    return storeEvents(conn);
+  }
+
+  return null;
 };
